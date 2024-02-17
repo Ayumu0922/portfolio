097f91f03961f0e12bf26787333a18569c51cf8f
Ayumu0922 <9bota.ayu6@yahoo.ne.jp>
2024-02-17 16:41:48 +0900

    edit project sesction

--- components/Project.tsx
+++ components/Project.tsx
@@ -3,7 +3,12 @@ import { motion } from "framer-motion";
 import Image from "next/image";
 import SectionTitle from "./SectionTitle";
 import { AiFillCaretRight } from "react-icons/ai";
-import { amazonImg, profileImage, studyOverview } from "@/public";
+import {
+  animationSite,
+  profileImage,
+  studyOverview,
+  transparentSite,
+} from "@/public";
 import { TbBrandGithub } from "react-icons/tb";
 import { RxOpenInNewWindow } from "react-icons/rx";
 
@@ -11,57 +16,60 @@ const Project = () => {
   return (
     <section id="project" className=" max-w-container mx-auto lgl:px-20 py-24">
       <SectionTitle title="作成したプロジェクト" titleNo="03" />
-      <div className=" w-full flex flex-col items-center justify-between gap-28 mt-10">
+      <div className=" w-full flex flex-col items-center justify-between gap-32 mt-10">
         {/* project 1 */}
         <div className=" w-full flex flex-col items-center justify-center gap-28 mt-10">
           <div className="flex flex-col xl:flex-row-reverse gap-6">
             {/* 動画を含むリンク */}
-            <a
-              className=" w-full xl:w-1/2 h-auto relative group"
-              href="プロジェクトのURL"
-              target="_blank"
-              rel="noopener noreferrer"
+
+            <div
+              className="relative max-w-xs overflow-hidden bg-cover bg-no-repeat md:w-[600px]"
+              data-te-ripple-init
+              data-te-ripple-color="light"
             >
-              <video
-                className=" w-full h-full object-contain"
-                src="./video/project1.mp4"
-                width={800}
-                controls
+              <Image
+                src={transparentSite}
+                alt=""
+                width={600}
+                className=" cursor-pointer "
               />
-            </a>
+              <a
+                href="https://transparent-website-sigma.vercel.app/"
+                target="_blank"
+              >
+                <div className="absolute bottom-0 left-0 right-0 top-0 h-full w-full overflow-hidden bg-[hsl(0,0%,98.4%,0.2)] bg-fixed opacity-0 transition duration-300 ease-in-out hover:opacity-100"></div>
+              </a>
+            </div>
+
             <div className=" w-full xl:w-1/2 flex flex-col gap-6 lgl:justify-between items-end text-left  z-10">
               <p className=" text-textGreen text-sm tracking-wide">
                 プロジェクト1
               </p>
-              <h3 className=" text-2xl font-bold">認証アプリケーション</h3>
+              <h3 className=" text-2xl font-bold">Login Form 1</h3>
               <p className=" bg-[#112240] text-sm md:text-base p-2 xl:-mr-12 md:p-6 rounded-xl ">
-                認証機能を <span className=" text-textGreen">Supabase</span>
-                をバックエンドに使用して実装しました。
-                Googleもしくは、Email,Passwordを使用してログインすることができます。
-                レイアウトには{" "}
+                モダンなアニメーションを使用したLogin Formを作成しました。
+                フレームワークには、
+                <span className=" text-textGreen">Next.js 14</span>
+                レイアウトには、
                 <span className=" text-textGreen">Tailwind CSS</span>
-                を使用しており、フレームワークは
-                <span className=" text-textGreen">Next.js14</span>
-                を使用しています。
+                を使用しています。画像をクリックしていただければサイトのURLに遷移します。{" "}
                 <span className=" text-textGreen"></span>
               </p>
               <ul className=" text-xs md:text-sm tracking-wide flex gap-2 md:gap-5 justify-between  text-textGreen">
                 <li>Next.js 14</li>
-                <li>Supabase</li>
                 <li>TailWind CSS</li>
-                <li>PostgresSQL</li>
               </ul>
               <div className=" text-2xl flex gap-4">
                 <a
                   className=" hover:text-textGreen duration-300"
-                  href="https://github.com"
+                  href="https://github.com/Ayumu0922/transparent-website"
                   target="_blank"
                 >
                   <TbBrandGithub />
                 </a>
                 <a
                   className=" hover:text-textGreen duration-300"
-                  href="https://github.com"
+                  href="https://transparent-website-sigma.vercel.app/"
                   target="_blank"
                 >
                   <RxOpenInNewWindow />
@@ -72,6 +80,63 @@ const Project = () => {
         </div>
       </div>
       {/* project two */}
+      <div className=" w-full flex flex-col items-center justify-center gap-28 mt-10">
+        <div className="flex flex-col xl:flex-row-reverse gap-6">
+          {/* 動画を含むリンク */}
+
+          <div
+            className="relative max-w-xs overflow-hidden bg-cover bg-no-repeat md:w-[600px]"
+            data-te-ripple-init
+            data-te-ripple-color="light"
+          >
+            <Image
+              src={animationSite}
+              alt=""
+              width={600}
+              className=" cursor-pointer "
+            />
+            <a href="https://ui-test-iota.vercel.app/" target="_blank">
+              <div className="absolute bottom-0 left-0 right-0 top-0 h-full w-full overflow-hidden bg-[hsl(0,0%,98.4%,0.2)] bg-fixed opacity-0 transition duration-300 ease-in-out hover:opacity-100"></div>
+            </a>
+          </div>
+
+          <div className=" w-full xl:w-1/2 flex flex-col gap-6 lgl:justify-between items-end text-left  z-10">
+            <p className=" text-textGreen text-sm tracking-wide">
+              プロジェクト2
+            </p>
+            <h3 className=" text-2xl font-bold">Login Form 1</h3>
+            <p className=" bg-[#112240] text-sm md:text-base p-2 xl:-mr-12 md:p-6 rounded-xl ">
+              背景にモダンなアニメーションを使用したLogin Formを作成しました。
+              フレームワークには、
+              <span className=" text-textGreen">Next.js 14</span>
+              レイアウトには、
+              <span className=" text-textGreen">Tailwind CSS</span>
+              を使用しています。画像をクリックしていただければサイトのURLに遷移します。{" "}
+              <span className=" text-textGreen"></span>
+            </p>
+            <ul className=" text-xs md:text-sm tracking-wide flex gap-2 md:gap-5 justify-between  text-textGreen">
+              <li>Next.js 14</li>
+              <li>TailWind CSS</li>
+            </ul>
+            <div className=" text-2xl flex gap-4">
+              <a
+                className=" hover:text-textGreen duration-300"
+                href="https://github.com/Ayumu0922/ui-test"
+                target="_blank"
+              >
+                <TbBrandGithub />
+              </a>
+              <a
+                className=" hover:text-textGreen duration-300"
+                href="https://ui-test-iota.vercel.app/"
+                target="_blank"
+              >
+                <RxOpenInNewWindow />
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
     </section>
   );
 };
--- public/index.ts
+++ public/index.ts
@@ -2,6 +2,15 @@ import logo from "./img/projects/logo2.webp";
 import profileImage from "./img/profileImage.jpg";
 import studyOverview from "./img/projects/study_overview.jpg";
 import studyPaper from "./img/projects/studyPaper.png";
-import amazonImg from "./img/projects/amazonClone.webp";
 
-export { logo, profileImage, studyOverview, studyPaper, amazonImg };
+import transparentSite from "./img/projects/transparent-site.png";
+import animationSite from "./img/projects/animation-site.png";
+
+export {
+  logo,
+  profileImage,
+  studyOverview,
+  studyPaper,
+  transparentSite,
+  animationSite,
+};
