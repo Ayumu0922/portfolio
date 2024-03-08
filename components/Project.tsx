4d9d9541c78b46caab17c854045ef6d20605eef3
Ayumu0922 <9bota.ayu6@yahoo.ne.jp>
2024-03-08 17:07:31 +0900

    add project 3

--- components/Project.tsx
+++ components/Project.tsx
@@ -3,7 +3,12 @@ import { motion } from "framer-motion";
 import Image from "next/image";
 import SectionTitle from "./SectionTitle";
 import { AiFillCaretRight } from "react-icons/ai";
-import { animationSite, transparentSite, imageStorageApp } from "@/public";
+import {
+  animationSite,
+  transparentSite,
+  imageStorageApp,
+  dicom_viewer,
+} from "@/public";
 import { TbBrandGithub } from "react-icons/tb";
 import { RxOpenInNewWindow } from "react-icons/rx";
 
@@ -40,7 +45,7 @@ const Project = () => {
               <p className=" text-textGreen text-sm tracking-wide">
                 プロジェクト1
               </p>
-              <h3 className=" text-2xl font-bold">Login Form 1</h3>
+              <h3 className=" text-2xl font-bold">Login Form</h3>
               <p className=" bg-[#112240] text-sm md:text-base p-2 xl:-mr-12 md:p-6 rounded-xl ">
                 ポップアップアニメーションをつけたLogin Formを作成しました。
                 LoginボタンをクリックしていただくとLogin
@@ -104,9 +109,7 @@ const Project = () => {
             <p className=" text-textGreen text-sm tracking-wide">
               プロジェクト2
             </p>
-            <h3 className=" text-2xl font-bold">
-              Image Storage app with supabase
-            </h3>
+            <h3 className=" text-2xl font-bold">Image Storage App </h3>
             <p className=" bg-[#112240] text-sm md:text-base p-2 xl:-mr-12 md:p-6 rounded-xl ">
               写真をアップロードして、保存できるアプリケーションを開発しました。アプリケーションに登録いただいた方は画像をアップロードすることが可能になります。フレームワークには、
               <span className=" text-textGreen">Next.js 14</span>
@@ -136,6 +139,68 @@ const Project = () => {
               >
                 <RxOpenInNewWindow />
               </a>
+            </div>
+          </div>
+        </div>
+        {/* project 3 */}
+        <div className=" w-full flex flex-col items-center justify-center gap-28 mt-10 mb-40">
+          <div className="flex flex-col xl:flex-row-reverse gap-6">
+            <div
+              className="relative max-w-xs overflow-hidden bg-cover bg-no-repeat md:w-[600px]"
+              data-te-ripple-init
+              data-te-ripple-color="light"
+            >
+              <Image
+                src={dicom_viewer}
+                alt=""
+                width={600}
+                className=" cursor-pointer "
+              />
+              <a
+                href="https://study-ui-henna.vercel.app/dashboard/patients"
+                target="_blank"
+              >
+                <div className="absolute bottom-0 left-0 right-0 top-0 h-full w-full overflow-hidden bg-[hsl(0,0%,98.4%,0.2)] bg-fixed opacity-0 transition duration-300 ease-in-out hover:opacity-100"></div>
+              </a>
+            </div>
+
+            <div className=" w-full xl:w-1/2 flex flex-col gap-6 lgl:justify-between items-end text-left  z-10">
+              <p className=" text-textGreen text-sm tracking-wide">
+                プロジェクト3
+              </p>
+              <h3 className=" text-2xl font-bold">医療診断支援システム</h3>
+              <p className=" bg-[#112240] text-sm md:text-base p-2 xl:-mr-12 md:p-6 rounded-xl ">
+                現在研究内容として開発した外反母趾自動検出アルゴリズムを実際にシステム化して現場に導入し利用していただくことを目的として作成しています。
+                医師の方が診断に介在できるようなインタラクティブな機能と患者さんの情報を一元管理できるような機能を搭載する予定です。
+                フレームワークには、
+                <span className=" text-textGreen">Next.js 14</span>
+                レイアウトには、
+                <span className=" text-textGreen">Tailwind CSS</span>,{" "}
+                <span className=" text-textGreen">chadcn/ui</span>
+                を使用しています。画像をクリックしていただければサイトのURLに遷移します。{" "}
+                <span className=" text-textGreen"></span>
+              </p>
+              <ul className=" text-xs md:text-sm tracking-wide flex gap-2 md:gap-5 justify-between  text-textGreen">
+                <li>Next.js 14</li>
+                <li>TailWind CSS</li>
+                <li>chadcn/ui</li>
+              </ul>
+              <div className=" text-2xl flex gap-4">
+                <a
+                  className=" hover:text-textGreen duration-300"
+                  href="https://github.com/Ayumu0922/study-ui"
+                  target="_blank"
+                >
+                  <TbBrandGithub />
+                </a>
+                <a
+                  className=" hover:text-textGreen duration-300"
+                  href="https://study-ui-henna.vercel.app/dashboard/patients"
+                  target="_blank"
+                >
+                  <RxOpenInNewWindow />
+                </a>
+              </div>
             </div>
           </div>
         </div>
--- public/index.ts
+++ public/index.ts
@@ -6,6 +6,7 @@ import studyPaper from "./img/projects/studyPaper.png";
 import transparentSite from "./img/projects/transparent-site.png";
 import animationSite from "./img/projects/animation-site.png";
 import imageStorageApp from "./img/projects/image_storage_app.png";
+import dicom_viewer from "./img/projects/dicom_viewer.png";
 
 export {
   logo,
@@ -15,4 +16,5 @@ export {
   transparentSite,
   animationSite,
   imageStorageApp,
+  dicom_viewer,
 };
